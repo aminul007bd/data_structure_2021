644e367a2644e155175fd569178e3f63de611165
aminul007bd <gem007bd@gmail.com>
2021-04-03 15:33:09 +0200

    linked list

--- src/com/turorial/exersice/Array.java
+++ src/com/turorial/exersice/Array.java
@@ -3,34 +3,39 @@ package com.turorial.exersice;
 public class Array {
     private int[] items;
     private int count;
+
     public Array(int length) {
         items = new int[length];
     }
+
     public void insert(int item) {
-        if(items.length == count) {
+        if (items.length == count) {
             int[] newItems = new int[count * 2];
-            for(int i = 0; i< items.length; i++) {
+            for (int i = 0; i < items.length; i++) {
                 newItems[i] = items[i];
             }
             items = newItems;
         }
-        items[count ++] = item;
+        items[count++] = item;
     }
+
     public void removeAt(int index) {
-        if(index < 0 || index > count)
+        if (index < 0 || index > count)
             throw new IllegalArgumentException();
-        for(int i = index; i < count; i++)
+        for (int i = index; i < count; i++)
             items[i] = items[i + 1];
         count--;
     }
+
     public int indexOf(int item) {
-        for(int i =0; i< count; i++)
-            if(items[i] == item)
+        for (int i = 0; i < count; i++)
+            if (items[i] == item)
                 return i;
         return -1;
     }
-    public void print(){
-        for (int i = 0; i< count; i++)
+
+    public void print() {
+        for (int i = 0; i < count; i++)
             System.out.println(items[i]);
     }
 }
---
+++ src/com/turorial/exersice/LinkedList.java
@@ -0,0 +1,149 @@
+package com.turorial.exersice;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private static class Node {
+        private final int value;
+        private Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+    private Node first;
+    private Node last;
+    private int size;
+
+    //addFirst
+    public void addFirst(int item) {
+        var node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+        size --;
+    }
+
+    //addLast
+    public void addLast(int item) {
+        var node = new Node(item);
+        if(isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+        size ++;
+    }
+
+    //removeFirst
+    public void removeFirst(int item) {
+        if(isEmpty())
+            throw new NoSuchElementException();
+        if(first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        size --;
+    }
+
+    //removeLast
+    public void removeLast(){
+        if(isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        if(first == last)
+          first = last = null;
+        else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size --;
+    }
+
+
+    //contains
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    //indexOf
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if(current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if(current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while (current != null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    // reverse the link list
+    public void reverse() {
+        if(isEmpty()) return;
+        var previous = first;
+        var current = first.next;
+
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        last = first;
+        last.next = null;
+        first = previous;
+    }
+
+    // find the kth node from the end
+    public int getKthFromTheEnd(int k) {
+        var a = first;
+        var b = first;
+        for(int i = 0; i < k -1; i++) {
+            b = b.next;
+            if(b == null)
+                throw new IllegalArgumentException();
+        }
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+        return a.value;
+    }
+
+}
